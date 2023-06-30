d71fc014e1297ce8554ae80ac19698dea2c77692
desuex <nnm043@gmail.com>
2023-07-01 03:19:44 +0600

    cURL demo

--- update_user_alice.sh
+++ update_user_alice.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 curl -X PUT -H "Content-Type: application/json" -d '{
-  "id": 1,
-  "name": "Alice",
-  "email": "alice@example.com",
+  "name": "AliceSmith123",
+  "email": "alice123@example.com",
   "created": "2023-06-30",
-  "deleted": null,
-  "notes": null
 }' http://localhost:8080/users/1
